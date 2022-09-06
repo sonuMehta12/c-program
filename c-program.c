515f4d28e2018a90913169e9934cec2ab11fe793
sonu mehta <72590811+sonuMehta12@users.noreply.github.com>
2022-09-06 20:16:40 +0530

    Update c-program.c

--- c-program.c
+++ c-program.c
@@ -1,1 +1,27 @@
+/////////printPyramid//////////////
+#include <stdio.h>
+
+void printPyramid(int number_of_rows){
+       int k = number_of_rows-1;
+    for(int i = 0; i < number_of_rows; i++){
+        for( int r = 0; r < k; r++){
+            printf("-");
+        }
+        k--;
+        for(int j =0; j  < i *2 +1 ; j++ ){
+            printf("*");
+        }
+        printf("\n");
+    } 
+}
+
+
+int main() {
+    // Write C code here
+
+    printPyramid(20);
+    return 0;
+}
+////////////////////END THE PROGRAM ///////////////////////////
+
 
